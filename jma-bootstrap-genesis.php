18357d0b4205525c7aa0e0f27fe353436af2698b
johnnya23 <johnantonacci23@gmail.com>
2019-12-27 16:00:24 -0700

    change to page input for header

--- jma-bootstrap-genesis.php
+++ jma-bootstrap-genesis.php
@@ -40,160 +40,27 @@ function jma_gbs_add_customizer()
 
 function jma_gbs_customizer($wp_customize)
 {
-    $wp_customize->add_section('jma_sample_custom_controls_section', array(
-        'title'      => __('Visible Section Name', 'mytheme'),
+    $wp_customize->add_section('jma_gbs_header_controls_section', array(
+        'title'      => __('Header', 'jma_gbs'),
         'priority'   => 30,
     ));
 
     $wp_customize->add_setting(
-            'jma_sample_sortable_repeater_control',
-            array(
-                'default' => '',
-                'transport' => 'refresh',
-            'sanitize_callback' => 'skyrocket_text_sanitization'
-            )
-        );
-    $wp_customize->add_control(new JMA_GBS_Sortable_Repeater_Custom_Control(
-            $wp_customize,
-            'jma_sample_sortable_repeater_control',
-            array(
-                'label' => __('Sortable Repeatersss'),
-                'description' => esc_html__('This is the control description.'),
-                'section' => 'jma_sample_custom_controls_section',
-            'input_attrs' => array(
-                'multiselect' => true,
-            ),
-            'choices' => array(
-                __('Antarctica', 'skyrocket') => array(
-                    'Antarctica/Casey' => __('Casey', 'skyrocket'),
-                    'Antarctica/Davis' => __('Davis', 'skyrocket'),
-                    'Antarctica/DumontDurville' => __('DumontDUrville', 'skyrocket'),
-                    'Antarctica/Macquarie' => __('Macquarie', 'skyrocket'),
-                    'Antarctica/Mawson' => __('Mawson', 'skyrocket'),
-                    'Antarctica/McMurdo' => __('McMurdo', 'skyrocket'),
-                    'Antarctica/Palmer' => __('Palmer', 'skyrocket'),
-                    'Antarctica/Rothera' => __('Rothera', 'skyrocket'),
-                    'Antarctica/Syowa' => __('Syowa', 'skyrocket'),
-                    'Antarctica/Troll' => __('Troll', 'skyrocket'),
-                    'Antarctica/Vostok' => __('Vostok', 'skyrocket'),
-                ),
-                __('Atlantic', 'skyrocket') => array(
-                    'Atlantic/Azores' => __('Azores', 'skyrocket'),
-                    'Atlantic/Bermuda' => __('Bermuda', 'skyrocket'),
-                    'Atlantic/Canary' => __('Canary', 'skyrocket'),
-                    'Atlantic/Cape_Verde' => __('Cape Verde', 'skyrocket'),
-                    'Atlantic/Faroe' => __('Faroe', 'skyrocket'),
-                    'Atlantic/Madeira' => __('Madeira', 'skyrocket'),
-                    'Atlantic/Reykjavik' => __('Reykjavik', 'skyrocket'),
-                    'Atlantic/South_Georgia' => __('South Georgia', 'skyrocket'),
-                    'Atlantic/Stanley' => __('Stanley', 'skyrocket'),
-                    'Atlantic/St_Helena' => __('St Helena', 'skyrocket'),
-                ),
-                __('Australia', 'skyrocket') => array(
-                    'Australia/Adelaide' => __('Adelaide', 'skyrocket'),
-                    'Australia/Brisbane' => __('Brisbane', 'skyrocket'),
-                    'Australia/Broken_Hill' => __('Broken Hill', 'skyrocket'),
-                    'Australia/Currie' => __('Currie', 'skyrocket'),
-                    'Australia/Darwin' => __('Darwin', 'skyrocket'),
-                    'Australia/Eucla' => __('Eucla', 'skyrocket'),
-                    'Australia/Hobart' => __('Hobart', 'skyrocket'),
-                    'Australia/Lindeman' => __('Lindeman', 'skyrocket'),
-                    'Australia/Lord_Howe' => __('Lord Howe', 'skyrocket'),
-                    'Australia/Melbourne' => __('Melbourne', 'skyrocket'),
-                    'Australia/Perth' => __('Perth', 'skyrocket'),
-                    'Australia/Sydney' => __('Sydney', 'skyrocket'),
-                )
-            ),
-                'button_labels' => array(
-                    'add' => __('Add Rowss'), // Optional. Button label for Add button. Default: Add
-                )
-            )
-        ));
-
-
-
-    $wp_customize->add_setting(
-        'jma_sample_sortable_input_repeater_control',
-    array(
-        'default' => '',
-        'transport' => 'refresh',
-        'sanitize_callback' => 'skyrocket_url_sanitization'
-    )
-);
-    $wp_customize->add_control(new Skyrocket_Sortable_Repeater_Custom_Control(
-    $wp_customize,
-    'jma_sample_sortable_input_repeater_control',
-    array(
-        'label' => __('Sortable Repeater'),
-        'description' => esc_html__('This is the control description.'),
-        'section' => 'jma_sample_custom_controls_section',
-        'button_labels' => array(
-            'add' => __('Add Row'), // Optional. Button label for Add button. Default: Add
+        'jma_gbs_header_page',
+        array(
+            'default' => '',
+            'transport' => 'refresh'
         )
-    )
-));
-
-
-    $wp_customize->add_setting(
-    'jma_sample_dropdown_select2_control_multi',
-    array(
-        'default' => array( 'Antarctica/McMurdo', 'Australia/Melbourne', 'Australia/Broken_Hill' ),
-        'transport' => 'refresh',
-        'sanitize_callback' => 'skyrocket_text_sanitization'
-    )
-);
-    $wp_customize->add_control(new Skyrocket_Dropdown_Select2_Custom_Control(
-    $wp_customize,
-    'jma_sample_dropdown_select2_control_multi',
-    array(
-        'label' => __('Dropdown Select2 Control', 'skyrocket'),
-        'description' => esc_html__('Sample Dropdown Select2 custom control (Multi-Select)', 'skyrocket'),
-        'section' => 'jma_sample_custom_controls_section',
-        'input_attrs' => array(
-            'multiselect' => true,
-        ),
-        'choices' => array(
-            __('Antarctica', 'skyrocket') => array(
-                'Antarctica/Casey' => __('Casey', 'skyrocket'),
-                'Antarctica/Davis' => __('Davis', 'skyrocket'),
-                'Antarctica/DumontDurville' => __('DumontDUrville', 'skyrocket'),
-                'Antarctica/Macquarie' => __('Macquarie', 'skyrocket'),
-                'Antarctica/Mawson' => __('Mawson', 'skyrocket'),
-                'Antarctica/McMurdo' => __('McMurdo', 'skyrocket'),
-                'Antarctica/Palmer' => __('Palmer', 'skyrocket'),
-                'Antarctica/Rothera' => __('Rothera', 'skyrocket'),
-                'Antarctica/Syowa' => __('Syowa', 'skyrocket'),
-                'Antarctica/Troll' => __('Troll', 'skyrocket'),
-                'Antarctica/Vostok' => __('Vostok', 'skyrocket'),
-            ),
-            __('Atlantic', 'skyrocket') => array(
-                'Atlantic/Azores' => __('Azores', 'skyrocket'),
-                'Atlantic/Bermuda' => __('Bermuda', 'skyrocket'),
-                'Atlantic/Canary' => __('Canary', 'skyrocket'),
-                'Atlantic/Cape_Verde' => __('Cape Verde', 'skyrocket'),
-                'Atlantic/Faroe' => __('Faroe', 'skyrocket'),
-                'Atlantic/Madeira' => __('Madeira', 'skyrocket'),
-                'Atlantic/Reykjavik' => __('Reykjavik', 'skyrocket'),
-                'Atlantic/South_Georgia' => __('South Georgia', 'skyrocket'),
-                'Atlantic/Stanley' => __('Stanley', 'skyrocket'),
-                'Atlantic/St_Helena' => __('St Helena', 'skyrocket'),
-            ),
-            __('Australia', 'skyrocket') => array(
-                'Australia/Adelaide' => __('Adelaide', 'skyrocket'),
-                'Australia/Brisbane' => __('Brisbane', 'skyrocket'),
-                'Australia/Broken_Hill' => __('Broken Hill', 'skyrocket'),
-                'Australia/Currie' => __('Currie', 'skyrocket'),
-                'Australia/Darwin' => __('Darwin', 'skyrocket'),
-                'Australia/Eucla' => __('Eucla', 'skyrocket'),
-                'Australia/Hobart' => __('Hobart', 'skyrocket'),
-                'Australia/Lindeman' => __('Lindeman', 'skyrocket'),
-                'Australia/Lord_Howe' => __('Lord Howe', 'skyrocket'),
-                'Australia/Melbourne' => __('Melbourne', 'skyrocket'),
-                'Australia/Perth' => __('Perth', 'skyrocket'),
-                'Australia/Sydney' => __('Sydney', 'skyrocket'),
+    );
+    $wp_customize->add_control(
+            'jma_gbs_header_page_control',
+            array(
+                'label' => __('Page for Header', 'jma_gbs'),
+                'description' => esc_html__('Page that will provide header content.'),
+                'section' => 'jma_gbs_header_controls_section',
+                'settings'   => 'jma_gbs_header_page',
+                'type' => 'dropdown-pages'
             )
-        )
-    )
-));
+    );
 }
-//add_action('customize_register', 'jma_gbs_customizer', 9999);
+add_action('customize_register', 'jma_gbs_customizer', 9999);
