60a78a21a0bd01d938435599714aeb74acd02c63
johnnya23 <johnantonacci23@gmail.com>
2021-03-27 08:44:58 -0600

    css to admin screen

--- bootstrap-genesis/jma_lib/load-assets.php
+++ bootstrap-genesis/jma_lib/load-assets.php
@@ -23,10 +23,8 @@ function JMA_GBS_enqueue_css_js()
 {
     $min = WP_DEBUG? '': '.min';
     // wp_enqueue_style( $handle, $src, $deps, $ver, $media );
-    wp_register_style('JMA_GBS_superfish_css', JMA_GBS_BASE_URI . 'dist/css/superfish' . $min . '.css', array(), JMA_GBS_VERSION);
-    wp_register_style('JMA_GBS_combined_css', JMA_GBS_BASE_URI . 'css/style.css', array(), JMA_GBS_VERSION);
-    wp_enqueue_style('JMA_GBS_superfish_css');
-    wp_enqueue_style('JMA_GBS_combined_css');
+    wp_enqueue_style('JMA_GBS_superfish_css', JMA_GBS_BASE_URI . 'dist/css/superfish' . $min . '.css', array(), JMA_GBS_VERSION);
+    wp_enqueue_style('JMA_GBS_combined_css', JMA_GBS_BASE_URI . 'css/style.css', array(), JMA_GBS_VERSION);
 
     // wp_enqueue_script( $handle, $src, $deps, $ver, $in_footer );
     // NOTE: this combined script is loading in the footer
@@ -56,12 +54,14 @@ add_action('wp_enqueue_scripts', 'JMA_GBS_enqueue_css_js');
 
 function JMA_GBS_admin_enqueue_css_js()
 {
-    wp_enqueue_style('JMA_GBS_superfish_css');
-    wp_enqueue_style('JMA_GBS_combined_css');
+    $min = WP_DEBUG? '': '.min';
+    wp_enqueue_style('JMA_GBS_superfish_css', JMA_GBS_BASE_URI . 'dist/css/superfish' . $min . '.css', array(), JMA_GBS_VERSION);
+    wp_enqueue_style('JMA_GBS_combined_css', JMA_GBS_BASE_URI . 'css/style.css', array(), JMA_GBS_VERSION);
 }
 add_action('admin_enqueue_scripts', 'JMA_GBS_admin_enqueue_css_js');
 
-function JMA_GBS_customizer_live_preview(){
+function JMA_GBS_customizer_live_preview()
+{
     jma_gbs_customize_save_after();
 }
 add_action('customize_preview_init', 'JMA_GBS_customizer_live_preview', -1);
