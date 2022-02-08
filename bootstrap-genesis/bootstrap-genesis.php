e2921ee790a405c0332ba466012db0fbfeabb8b2
johnnya23 <johnantonacci23@gmail.com>
2022-02-07 19:59:02 -0700

    force non-full screen editing

--- bootstrap-genesis/bootstrap-genesis.php
+++ bootstrap-genesis/bootstrap-genesis.php
@@ -157,3 +157,12 @@ function jma_gbs_reuseable_url_function()
     $menu[7][2] = site_url('wp-admin/edit.php?post_type=wp_block');
 }
 add_action('admin_menu', 'jma_gbs_reuseable_url_function');
+
+if (is_admin()) {
+    function jma_gbs_disable_editor_fullscreen_by_default()
+    {
+        $script = "window.addEventListener('load', (event) => { const isFullscreenMode = wp.data.select( 'core/edit-post' ).isFeatureActive( 'fullscreenMode' ); if ( isFullscreenMode ) { wp.data.dispatch( 'core/edit-post' ).toggleFeature( 'fullscreenMode' ); } });";
+        wp_add_inline_script('wp-blocks', $script);
+    }
+    add_action('enqueue_block_editor_assets', 'jma_gbs_disable_editor_fullscreen_by_default');
+}
